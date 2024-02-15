f9d471c8ce46b7ad0e4bca18b5e9c6a0921d41f4
Kisu kapadia <kisukapadia@gmail.com>
2024-02-15 11:45:03 +0530

    success

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-set -e
-
-containerid=$(sudo docker ps -q)
-
-if [ -n "$containerid" ]; then 
-    sudo docker rm -f "$containerid"
-    sudo docker rmi $(docker images -f "dangling=true" -q)
-else
-    echo "No container running"
-fi 
+sudo docker rm -f $(sudo docker ps -q)
+sudo docker rmi $(docker images -f "dangling=true" -q)
