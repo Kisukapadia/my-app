9e20f372557eafcbdc3c84a9ddaa64abdfc636cd
Kisu kapadia <kisukapadia@gmail.com>
2024-02-21 16:23:00 +0530

    success

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ function App() {
           React 
         </a>
       </header>
+import * as React from 'react';  
+import Stack from '@mui/material/Stack';  
+import Button from '@mui/material/Button';  
+export default function BasicButtons() 
+{  
+  return (  
+    <Stack spacing={2} direction="row">  
+    <Button variant="contained">Contained</Button>  
+    </Stack>  
+  );  
+}  
     </div>
   );
 }
