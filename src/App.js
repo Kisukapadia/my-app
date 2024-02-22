43ecbd50043fe28f0cd8d1a3690358e5435436cd
Kisu kapadia <kisukapadia@gmail.com>
2024-02-22 12:02:08 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Count from './Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Count/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,14 +20,8 @@ function App() {
         >
           React App
         </a>
-      </header>
-       <div>
-        < Button theme="blue" href="https://reactjs.org">
-          React Button
-        </Button>
-      </div>
+      </header> */}
     </div>
   );
 }
-
 export default App;
