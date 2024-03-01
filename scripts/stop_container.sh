6ad5501b23a57c8e8162fa60c605a5d2cdec695f
Kisu kapadia <kisukapadia@gmail.com>
2024-03-01 12:56:25 +0530

    success

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,15 @@
 set -e
 
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo docker rmi $(docker images -f "dangling=true" -q)
+containerid=$(sudo docker ps -q)
+imageid=$(docker images -f "dangling=true" -q)
+if [ -n "$imageid" ]; then
+        sudo docker rmi $(docker images -f "dangling=true" -q)
+else
+        echo "No images found"
+fi
+if [ -n "$containerid" ]; then
+    
+    sudo docker rm -f $(sudo docker ps -a -q)
+else 
+    echo "No container running"
+fi
