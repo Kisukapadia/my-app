328dc343cb9593239edfc85b7a10b911fe89eb97
Kisu kapadia <kisukapadia@gmail.com>
2024-03-07 10:37:25 +0530

    success

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Site   
+          React App
         </a>  
       </header>
       <Count/>
